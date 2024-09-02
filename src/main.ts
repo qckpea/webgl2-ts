32f8ced0136778bcf70b98ba5970e749dafd21dd
Norbert Csortos <csortosnorbert@gmail.com>
2024-09-02 22:04:02 +0200

    init repo

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+const main = () => {
+  const canvas = document.querySelector("#canvas") as HTMLCanvasElement;
+  const gl = canvas.getContext("webgl2") as WebGL2RenderingContext;
+  if (!gl) {
+    console.error("Could not initialize WebGL2");
+    return;
+  }
+};
+
+main();
